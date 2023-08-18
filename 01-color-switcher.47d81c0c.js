6d36fe6064f4e720937b7f6d092a98fae6b4b3f5
OlgaSharhorodska <128105592+OlgaSharhorodska@users.noreply.github.com>
2023-08-18 19:01:18 +0000

    Deploying to gh-pages from @ OlgaSharhorodska/goit-js-hw-09@1971ecfd9cdd34f74c954e9d0786a0f4ffff80c3 🚀

---
+++ 01-color-switcher.47d81c0c.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");let a=null;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.47d81c0c.js.map
